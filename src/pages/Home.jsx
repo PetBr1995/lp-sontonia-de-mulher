682080c306f4c88d52646226a5583ae81d90d35b
PetBr1995 <dev.petersonbrito@gmail.com>
2026-03-03 09:30:57 -0400

    Ajustes de SER e favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,11 @@ const Home = () => {
         <FifteenthSection />
         <SixteenthSection />
         <FormSection />
-
+        <footer className="bg-[#9E2F3F] px-4 py-6">
+          <p className="text-center text-sm text-white sm:text-base">
+            Todos os direitos reservados
+          </p>
+        </footer>
       </section>
     </>
   );
