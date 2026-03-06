857e8f30998eeef485cba2b02049b1e431f60e03
PetBr1995 <dev.petersonbrito@gmail.com>
2026-03-06 13:39:23 -0400

    Ajustes

--- src/components/Passaportes.jsx
+++ src/components/Passaportes.jsx
@@ -102,7 +102,7 @@ const Passaportes = () => {
                                                 href="#form"
                                                 className="block text-center text-base font-light text-white transition hover:brightness-110 sm:text-lg lg:text-xl"
                                             >
-                                                Quero garantir minha vaga
+                                                teste Quero garantir minha vaga
                                             </a>
                                         </div>
                                     </div>
